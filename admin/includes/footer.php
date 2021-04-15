46c098f6bcd7a6b5078c6b51bba92b54bfc5560d
BuildTools <unconfigured@null.spigotmc.org>
2021-04-15 08:08:36 +0530

    first-commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>2021 Brought to You By Team 11 (Soham, Deepesh, Shakti, Aakar and Parshav)</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>2021 Brought to You By Team 11 (Soham, Deepesh, Shakti, Aakar and Parshav)</a></strong>
+    </div>
+</footer>
